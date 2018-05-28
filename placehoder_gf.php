c0bf5f743b9ce6ecb61a0616da7e67e584f578a9
Corporación Educativa Continental <gabrielgonzales@Gabriel-2.local>
2018-05-28 15:18:19 -0500

    Fusion
    Enable Gravity Forms Placeholders and Fix Gravity Form Tabindex
    Conflicts

--- placehoder_gf.php
+++ placehoder_gf.php
@@ -1,15 +1,32 @@
 <?php
 /*
-Plugin Name: Enable Gravity Forms Placeholders
+Plugin Name: Enable Placeholders, Tabindex Conflicts
 Plugin URI: http://oc.continental.edu.pe
-Description: Enable placeholders in Gravity Forms
-Version: 0.1.1
+Description: Enable placeholders in Gravity Forms and Fix Gravity Form Tabindex Conflicts
+Version: 0.2
 Author: Oficina de Comunicaciones de la Corporacion Educativa continental
 Author URI: http://oc.continental.edu.pe
 Instructions: Active plugin.
 */
 
 if (class_exists('GFForms')) {
+	/**
+	* Enable Gravity Forms Placeholders
+	*/
     add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
+
+	/**
+	 * Fix Gravity Form Tabindex Conflicts
+	 * https://gravitywiz.com/fix-gravity-form-tabindex-conflicts/
+	 */
+	add_filter( 'gform_tabindex', 'gform_tabindexer', 10, 2 );
+	function gform_tabindexer( $tab_index, $form = false ) {
+	    $starting_index = 1000; // if you need a higher tabindex, update this number
+	    if( $form )
+	        add_filter( 'gform_tabindex_' . $form['id'], 'gform_tabindexer' );
+	    return GFCommon::$tab_index >= $starting_index ? GFCommon::$tab_index : $starting_index;
+	}
+
+
 }
 
